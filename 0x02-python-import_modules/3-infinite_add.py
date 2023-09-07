5d25c44446d6a3c23b9674037235286b1d2c5aa1
assem.hifny.aboelhamd@gmail.com <assem.hifny.aboelhamd@gmail.com>
2023-09-07 15:28:54 -0700

    tasks 2->5

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    
+    import sys
+    arg_len = sys.argv[1:]
+    result = 0
+
+    for arg in arg_len:
+        result += int(arg)
+
+    print(result)
