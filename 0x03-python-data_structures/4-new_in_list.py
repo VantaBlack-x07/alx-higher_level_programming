9a1f15279e173ee282f8232733b4672e66abd90e
assem.hifny.aboelhamd@gmail.com <assem.hifny.aboelhamd@gmail.com>
2023-09-10 10:09:58 -0700

    tasks 2->5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list[:]
+    if idx < 0 or len(my_list) - 1 < idx:
+        return list_cpy
+    else:
+        list_cpy[idx] = element
+        return list_cpy
