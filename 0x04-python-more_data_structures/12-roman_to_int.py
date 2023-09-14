d2551b296f159074e2a81a8dbd02c597ccfaa0f2
assem.hifny.aboelhamd@gmail.com <assem.hifny.aboelhamd@gmail.com>
2023-09-13 18:08:32 -0700

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not roman_string:
+
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
 
     roman_numerals = {
