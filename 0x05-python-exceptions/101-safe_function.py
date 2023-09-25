c5a992456013e05309791f3f29b77728e3a6b816
assem.hifny.aboelhamd@gmail.com <assem.hifny.aboelhamd@gmail.com>
2023-09-25 14:30:22 -0700

    advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {:s}".format(str(e)), file=sys.stderr)
+        return Nonei
