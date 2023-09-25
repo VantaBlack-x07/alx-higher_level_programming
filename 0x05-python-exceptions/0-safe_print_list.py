ca974f766d75137d63b3968271cc8453f0840343
assem.hifny.aboelhamd@gmail.com <assem.hifny.aboelhamd@gmail.com>
2023-09-25 14:13:13 -0700

    tasks 0->6

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    idx = 0
+    try:
+        while idx < x:
+            print(my_list[idx], end="")
+            idx += 1
+    except IndexError:
+        print()
+        return idx
+
+    print()
+    return idx
